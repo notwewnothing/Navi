ba617c77fc2051377141371a8c7edc67ac7ab202
notwewnothing <notwewnothing@gmail.com>
2026-08-14 18:59:47 +0100

    Making production changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:navi_/main.dart';
+import 'package:navi/main.dart';
 
 void main() {
   testWidgets('NaviApp renders splash screen', (WidgetTester tester) async {
